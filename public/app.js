0a5fedf110c7a6f5254c466b0e1bb36f170cb6c5
EC2 Default User <ec2-user@ip-172-31-31-179.us-east-2.compute.internal>
2020-12-16 03:34:10 +0000

    add ability to add and delete todo items from database

--- public/app.js
+++ public/app.js
@@ -1,13 +1,53 @@
 $(document).ready(function(){
-    $.getJson("/api/todos"){
-        .then(function(addTodos)
+    $.getJSON("/api/todos")
+        .then(addTodos)
+        
+    $('#todoInput').keypress(function(event){
+        if(event.which == 13) {
+        createTodo();
+        }
+    });
+    $('.list').on('click', 'span', function() {
+        removeTodo($(this).parent());
     })
 });
 
 function addTodos(todos) {
     //add todos to the page
     todos.forEach(function(todo){
-        var newTodo = $('<li class="task">'+todo.name + '<li>');
-        $('.list').append(newTodo);
+    });
+}
+
+function addTodo(todo){
+     var newTodo = $('<li class="task">'+todo.name +' <span>X</span></li>');
+     newTodo.data('id', todo._id);
+    if(todo.completed){
+        newTodo.addClass("done");
+    }
+    $('.list').append(newTodo);
+}
+
+function createTodo(){
+    //send request to create new todo
+    var usrInput = $('#todoInput').val();
+    $.post('/api/todos',{name: usrInput })
+    .then(function(newTodo){
+         $('#todoInput').val('')
+        addTodo(newTodo);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
+
+function removeTodo(todo){
+    var clickedId = $(this).parent().data('id');
+    var deleteUrl = '/api/todos/' + clickedId;
+    $.ajax({
+        method: 'DELETE',
+        url: deleteUrl
+    })
+    .then(function(data){
+        todo.remove();
     })
 }
